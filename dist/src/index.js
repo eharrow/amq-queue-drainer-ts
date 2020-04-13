b10ad787bf1e80c0bb9789ed2a1b368fec19f7d1
Ewan Harrow <ewan@harrow.org>
2020-04-13 19:18:57 +0100

    style: a bit of formatting

---
+++ dist/src/index.js
@@ -0,0 +1,62 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const chalk_1 = __importDefault(require("chalk"));
+const figlet_1 = __importDefault(require("figlet"));
+const commander_1 = __importDefault(require("commander"));
+const drainer_1 = require("./drainer");
+const package_json_1 = __importDefault(require("../package.json"));
+const i18n_1 = __importDefault(require("i18n"));
+i18n_1.default.configure({
+    directory: __dirname + "/locales",
+    objectNotation: true,
+});
+// example queue names
+const QUEUE_1 = "queue_1";
+const QUEUE_2 = "queue_2";
+const QUEUE_3 = "queue_3";
+const fonts = figlet_1.default.fontsSync();
+const font = fonts[Math.floor(Math.random() * fonts.length)];
+console.log(chalk_1.default.yellow(figlet_1.default.textSync("Q Drainer", { font, horizontalLayout: "full" })));
+commander_1.default
+    .version(package_json_1.default.version)
+    .option("-h, --host <host>", "rabbit host", "localhost")
+    .option("-p, --port <port>", "rabbit port", "5672")
+    .option("-v, --vhost <vhost>", "virtual host", "")
+    .option("-u, --user <user>", "rabbit user")
+    .option("-c, --password <password>", "rabbit password")
+    .option("-q, --queue <queue>", `queue name e.g. ${QUEUE_1}, ${QUEUE_2} or ${QUEUE_3}`, "test_q")
+    .option("-l, --log-message", "log the dequeued message.  This will be ignored if used with --log-message-csv")
+    .option("--log-message-csv", "log the dequeued message as CSV")
+    .option("-n, --num-to-consume [num>", "The number of messages to consume and if absent consume all")
+    .parse(process.argv);
+const host = commander_1.default.host;
+const port = commander_1.default.port;
+const user = commander_1.default.user;
+const password = commander_1.default.password;
+const vhost = commander_1.default.vhost;
+const queue = commander_1.default.queue;
+const logMessage = commander_1.default.logMessage;
+const logMessageCsv = commander_1.default.logMessageCsv;
+const numToConsume = commander_1.default.numToConsume || 0;
+if (!(host && port && queue)) {
+    console.error(i18n_1.default.__("err_missingArgs"));
+    console.error(commander_1.default.help());
+    process.exit(1);
+}
+let url;
+if (user && password) {
+    url = `amqp://${user}:${password}@${host}:${port}`.concat(vhost ? `/${vhost}` : "");
+}
+else {
+    url = `amqp://${host}:${port}`.concat(vhost ? `/${vhost}` : "");
+}
+const drainer = new drainer_1.Drainer(url, queue, logMessage, logMessageCsv, i18n_1.default);
+if (numToConsume === 0) {
+    drainer.consumeMessages().catch(err => console.info('oh oh'));
+}
+else {
+    drainer.consumeNmessages(numToConsume).catch(err => console.info('oh oh'));
+}
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,48 @@
-import chalk from 'chalk';
-import figlet from 'figlet';
-import program from 'commander';
-import { Drainer } from './drainer';
-import pkg from '../package.json';
-import i18n from 'i18n';
+import chalk from "chalk";
+import figlet from "figlet";
+import program from "commander";
+import { Drainer } from "./drainer";
+import pkg from "../package.json";
+import i18n from "i18n";
 
 i18n.configure({
-    directory: __dirname + '/locales',
-    objectNotation: true
+  directory: __dirname + "/locales",
+  objectNotation: true,
 });
 
 // example queue names
-const QUEUE_1 = 'queue_1';
-const QUEUE_2 = 'queue_2';
-const QUEUE_3 = 'queue_3';
+const QUEUE_1 = "queue_1";
+const QUEUE_2 = "queue_2";
+const QUEUE_3 = "queue_3";
 
 const fonts = figlet.fontsSync();
 const font = fonts[Math.floor(Math.random() * fonts.length)];
-console.log(chalk.yellow(figlet.textSync('Q Drainer', { font, horizontalLayout: 'full' })));
+console.log(
+  chalk.yellow(figlet.textSync("Q Drainer", { font, horizontalLayout: "full" }))
+);
 
 program
-    .version(pkg.version)
-    .option('-h, --host <host>', 'rabbit host', 'localhost')
-    .option('-p, --port <port>', 'rabbit port', '5672')
-    .option('-v, --vhost <vhost>', 'virtual host', '')
-    .option('-u, --user <user>', 'rabbit user')
-    .option('-c, --password <password>', 'rabbit password')
-    .option(
-        '-q, --queue <queue>',
-        `queue name e.g. ${QUEUE_1}, ${QUEUE_2} or ${QUEUE_3}`, 'test_q'
-    )
-    .option('-l, --log-message', 'log the dequeued message.  This will be ignored if used with --log-message-csv')
-    .option('--log-message-csv', 'log the dequeued message as CSV')
-    .option('-n, --num-to-consume [num>', 'The number of messages to consume and if absent consume all')
-    .parse(process.argv);
+  .version(pkg.version)
+  .option("-h, --host <host>", "rabbit host", "localhost")
+  .option("-p, --port <port>", "rabbit port", "5672")
+  .option("-v, --vhost <vhost>", "virtual host", "")
+  .option("-u, --user <user>", "rabbit user")
+  .option("-c, --password <password>", "rabbit password")
+  .option(
+    "-q, --queue <queue>",
+    `queue name e.g. ${QUEUE_1}, ${QUEUE_2} or ${QUEUE_3}`,
+    "test_q"
+  )
+  .option(
+    "-l, --log-message",
+    "log the dequeued message.  This will be ignored if used with --log-message-csv"
+  )
+  .option("--log-message-csv", "log the dequeued message as CSV")
+  .option(
+    "-n, --num-to-consume [num>",
+    "The number of messages to consume and if absent consume all"
+  )
+  .parse(process.argv);
 
 const host: string = program.host;
 const port: string = program.port;
@@ -46,22 +55,31 @@ const logMessageCsv: boolean = program.logMessageCsv;
 const numToConsume: number = program.numToConsume || 0;
 
 if (!(host && port && queue)) {
-    console.error(i18n.__('err_missingArgs'));
-    console.error(program.help());
-    process.exit(1);
+  console.error(i18n.__("err_missingArgs"));
+  console.error(program.help());
+  process.exit(1);
 }
 
 let url;
 
 if (user && password) {
-    url = `amqp://${user}:${password}@${host}:${port}`.concat(vhost ? `/${vhost}` : '');
+  url = `amqp://${user}:${password}@${host}:${port}`.concat(
+    vhost ? `/${vhost}` : ""
+  );
 } else {
-    url = `amqp://${host}:${port}`.concat(vhost ? `/${vhost}` : '');
+  url = `amqp://${host}:${port}`.concat(vhost ? `/${vhost}` : "");
 }
 
-const drainer: Drainer = new Drainer(url, queue, logMessage, logMessageCsv, i18n);
+const drainer: Drainer = new Drainer(
+  url,
+  queue,
+  logMessage,
+  logMessageCsv,
+  i18n
+);
 if (numToConsume === 0) {
-    drainer.consumeMessages();
+  drainer.consumeMessages().catch(err => console.info('oh oh'));
 } else {
-    drainer.consumeNmessages(numToConsume);
+  drainer.consumeNmessages(numToConsume).catch(err => console.info('oh oh'));
 }
+
