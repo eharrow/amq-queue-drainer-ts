df93fd57a10dc0c45a0d2c5018d3a6866d442f92
Ewan Harrow <ewan@harrow.org>
2023-05-30 12:51:25 +0100

    chore: npm update

--- dist/src/drainer.js
+++ dist/src/drainer.js
@@ -12,6 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Drainer = void 0;
 const cli_spinner_1 = require("cli-spinner");
 const chalk_1 = __importDefault(require("chalk"));
 const node_emoji_1 = __importDefault(require("node-emoji"));
