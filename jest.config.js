ac140e9a08464db60a15653f3873963ac89defc2
Ewan Harrow <ewan@harrow.org>
2023-06-07 10:47:54 +0100

    test: spin up rabbit, send message and consume

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    "node_modules/chalk/.+\\.(j|t)sx?$": "ts-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!chalk/.*)"
+  ]
 };
--- tests/drain.test.ts
+++ tests/drain.test.ts
@@ -5,8 +5,11 @@ import {
   GenericContainer
 } from "testcontainers";
 import { describe, expect, test } from '@jest/globals';
+import * as amqplib from "amqplib";
+import { Drainer } from "../src/drainer";
+import i18n from "i18n";
 
-
+jest.mock('i18n');
 
 describe('basic test', () => {
   const container: TestContainer = new GenericContainer("rabbitmq:3-management");
@@ -14,11 +17,39 @@ describe('basic test', () => {
   let stoppedContainer: StoppedTestContainer;
 
   beforeAll(async () => {
-    startedContainer = await container.start();
+    startedContainer = await container.withExposedPorts(5672).start();
+  }, 10000);
+
+  afterAll(async () => {
     stoppedContainer = await startedContainer.stop();
   });
 
-  test('adds 1 + 2 to equal 3', () => {
+  test('adds 1 + 2 to equal 3', async () => {
+    const host: string = startedContainer.getHost();
+    const port = startedContainer.getFirstMappedPort();
+    console.log(`host ${host}, port ${port}`);
+    const url = `amqp://${host}:${port}`;
+    const connection = await amqplib.connect(url);
+    const ch = await connection.createChannel();
+    const q = 'hello';
+    const msg = 'Hello World!';
+
+    ch.assertQueue(q, { durable: false });
+    ch.sendToQueue(q, Buffer.from(msg));
+    console.log(' [x] Sent %s', msg);
+
+    const drainer: Drainer = new Drainer(
+      url,
+      q,
+      true,
+      false,
+      i18n
+    );
+
+    drainer
+      .consumeMessages()
+      .catch((err) => console.info("oh oh"));
+
     expect(1 + 2).toBe(3);
-  });
+  }, 5000);
 });
