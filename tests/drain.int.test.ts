9cc0c4d28e24411388f574fb2b3df7187d40b63e
Ewan Harrow <ewan@harrow.org>
2023-07-25 10:37:33 +0100

    test: initial non-working test

--- tests/drain.int.test.ts
+++ tests/drain.int.test.ts
@@ -7,6 +7,8 @@ import {
 import { describe, expect, test } from '@jest/globals';
 import * as amqplib from "amqplib";
 import { Drainer } from "../src/drainer";
+import { Filler } from "../src/filler";
+
 import i18n from "i18n";
 
 jest.mock('i18n');
@@ -14,9 +16,21 @@ jest.mock('i18n');
 describe('basic test', () => {
   const container: TestContainer = new GenericContainer("rabbitmq:3-management");
   let startedContainer: StartedTestContainer;
+  let host: string;
+  let port: number;
+  let url: string;
+  let ch: amqplib.Channel;
+  const q = 'hello';
+
 
   beforeAll(async () => {
     startedContainer = await container.withExposedPorts(5672).start();
+    host = startedContainer.getHost();
+    port = startedContainer.getFirstMappedPort();
+    url = `amqp://${host}:${port}`;
+    const connection = await amqplib.connect(url);
+    ch = await connection.createChannel();
+    ch.assertQueue(q, { durable: false });
   }, 60000);
 
   afterAll(async () => {
@@ -24,20 +38,12 @@ describe('basic test', () => {
   }, 60000);
 
   test('it consumes a message from the queue', async () => {
-      const host: string = startedContainer.getHost();
-      const port = startedContainer.getFirstMappedPort();
-      // console.log(`host ${host}, port ${port}`);
-    const url = `amqp://${host}:${port}`;
-      const connection = await amqplib.connect(url);
-    const ch = await connection.createChannel();
-    const q = 'hello';
-      const msg = 'Hello World!';
-  
-      ch.assertQueue(q, { durable: false });
-      for (let index = 0; index < 100; index++) {
-        ch.sendToQueue(q, Buffer.from(msg));
-        // console.log(` [${index}] Sent %s`, msg);
-      }
+    const msg = 'Hello World!';
+
+    for (let index = 0; index < 100; index++) {
+      ch.sendToQueue(q, Buffer.from(msg));
+      // console.log(` [${index}] Sent %s`, msg);
+    }
 
     const drainer: Drainer = new Drainer(
       url,
@@ -53,4 +59,20 @@ describe('basic test', () => {
     // console.log(`purging queue of ${msgCount} messages`);
     expect(msgCount).toBe(99);
   }, 5000);
+
+  test('it publishes a message to the queue', async () => {
+    const msg = 'Hello World!';
+
+    for (let index = 0; index < 100; index++) {
+      ch.sendToQueue(q, Buffer.from(msg));
+      // console.log(` [${index}] Sent %s`, msg);
+    }
+    const filler: Filler = new Filler(
+      url,
+      q,
+      true,
+      false,
+      i18n
+    );
+  await filler.publishMessage(msg);
 });
